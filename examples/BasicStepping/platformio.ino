93c2cd584da6b5f7e28bfd8090917253314824e0
Greg Lewin <gcl8a@icloud.com>
2025-01-18 17:56:02 -0500

    reorg for pio

---
+++ examples/BasicStepping/platformio.ino
@@ -0,0 +1,11 @@
+
+[env:a-star32U4]
+
+platform = atmelavr
+board = a-star32U4
+framework = arduino
+
+monitor_speed = 115200
+
+lib_deps = 
+    /Users/greg/Documents/PlatformIO/device-lib/amis-30543-pololu
